b71e2841be9eeda278d65ea18f9d9d451470d0ec
kdotcdot <k.c.young1@gmail.com>
2015-07-04 15:34:21 -0400

    Added own code to views.py

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -8,7 +9,35 @@ from .models import Greeting
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
     print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    bill = compute_bill("apple", "banana", "orange")
+    times = int(os.environ.get('TIMES',3))
+    #return HttpResponse('Hello! ' * times)
+    return HttpResponse('<pre>' + r.text + bill + '</pre>')
+    
+    shopping_list = ["banana", "orange", "apple"]
+
+	stock = {
+		"banana": 6,
+		"apple": 0,
+		"orange": 32,
+		"pear": 15
+	}
+		
+	prices = {
+		"banana": 4,
+		"apple": 2,
+		"orange": 1.5,
+		"pear": 3
+	}
+	
+	# Write your code below!
+	def compute_bill(food):
+		total = 0
+		for item in food:
+			if stock[item] > 0:
+				total = total + prices[item]
+				stock[item] -= 1
+		return total
 
 def db(request):
 
