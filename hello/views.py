44877db433fa935be7a98b071dd21c1cf7d6bddf
kdotcdot <k.c.young1@gmail.com>
2015-07-04 16:01:26 -0400

    Testing DB migration

--- hello/views.py
+++ hello/views.py
@@ -5,39 +5,37 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+stock = {
+	"banana": 6,
+	"apple": 0,
+	"orange": 32,
+	"pear": 15
+}
+	
+prices = {
+	"banana": 4,
+	"apple": 2,
+	"orange": 1.5,
+	"pear": 3
+}
+
+#Write your code below!
+def compute_bill(food):
+	total = 0
+	for item in food:
+		if stock[item] > 0:
+			total = total + prices[item]
+			stock[item] -= 1
+	return total
+	
+food_order = ["apple", "orange"]
+
 # Create your views here.
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    bill = compute_bill("apple", "banana", "orange")
+    #print r.text
     times = int(os.environ.get('TIMES',3))
-    #return HttpResponse('Hello! ' * times)
-    return HttpResponse('<pre>' + r.text + bill + '</pre>')
-    
-    shopping_list = ["banana", "orange", "apple"]
-
-	stock = {
-		"banana": 6,
-		"apple": 0,
-		"orange": 32,
-		"pear": 15
-	}
-		
-	prices = {
-		"banana": 4,
-		"apple": 2,
-		"orange": 1.5,
-		"pear": 3
-	}
-	
-	# Write your code below!
-	def compute_bill(food):
-		total = 0
-		for item in food:
-			if stock[item] > 0:
-				total = total + prices[item]
-				stock[item] -= 1
-		return total
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
 
