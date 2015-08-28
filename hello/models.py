2dd9b4d9bebb917868d495c0e1e3aa59d172ba87
kdotcdot <k.c.young1@gmail.com>
2015-08-28 11:06:33 -0400

    Unclear - uncommitted

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,23 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+#Create a table for projects and to-dos
+#class Project(models.Model):
+# 	name = models.CharField(max_length=50)
+# 	owner = models.CharField(max_length=50)
+# 
+# class Todo(models.Model):
+# 	project_name = models.ForeignKey(Project)
+# 	title = models.CharField(max_length=100)
+# 	due_date = models.DateField()
+# 	order = models.IntegerField()
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- hello/views.py
+++ hello/views.py
@@ -5,44 +5,24 @@ import psycopg2
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Greeting
-
-stock = {
-	"banana": 6,
-	"apple": 0,
-	"orange": 32,
-	"pear": 15
-}
-	
-prices = {
-	"banana": 4,
-	"apple": 2,
-	"orange": 1.5,
-	"pear": 3
-}
-
-#Write your code below!
-def compute_bill(food):
-	total = 0
-	for item in food:
-		if stock[item] > 0:
-			total = total + prices[item]
-			stock[item] -= 1
-	return total
-	
-food_order = ["apple", "orange"]
+from .models import Question
+from .models import Choice
 
 # Create your views here.
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
-    #print r.text
+    print r.text
     times = int(os.environ.get('TIMES',3))
     return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
     greeting = Greeting()
     greeting.save()
-
     greetings = Greeting.objects.all()
-
+    
+    #p = Question(question_text="Blade Project", pub_date="KC")
+    
+    #p.save()
+	#projects = Project.objects.all()
+	
     return render(request, 'db.html', {'greetings': greetings})
-
